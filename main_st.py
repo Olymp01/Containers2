96a2b6d010d66eb36dcc68c0f3f99f2b1c2f83e5
Olymp01 <141898960+Olymp01@users.noreply.github.com>
2023-08-11 14:46:54 +0600

    Add files via upload

---
+++ main_st.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import streamlit as st
+from io import BytesIO
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+import psycopg2
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from Autorization;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
+
+
+
+
+        
+
+
