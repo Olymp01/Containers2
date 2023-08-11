fc49e9753d509252a2c5baabb777cb3bb9adaa5e
Olymp01 <141898960+Olymp01@users.noreply.github.com>
2023-08-11 14:57:47 +0600

    Update main_st.py

--- main_st.py
+++ main_st.py
@@ -4,23 +4,17 @@ from io import BytesIO
 from datetime import datetime
 import psycopg2
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
 conn = init_connection()
 
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from Autorization;")
+rows = run_query("SELECT * from autorization;")
 
 # Print results.
 for row in rows:
